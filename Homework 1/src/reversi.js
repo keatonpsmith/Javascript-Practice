b945407cd8741468a2ad9308956e51fb6ff7810c
Keaton Smith <keatonpsmith@gmail.com>
2019-01-13 16:19:29 -0500

    Added tests

---
+++ Homework 1/src/reversi.js
@@ -0,0 +1,15 @@
+function repeat(ele, n) {
+    // implementation
+} 
+    
+function generateBoard(rows, cols, initialValue) {
+    // implementation
+} 
+
+// ...
+
+module.exports = {
+    repeat: repeat,
+    generateBoard: generateBoard,
+    // ...
+}
