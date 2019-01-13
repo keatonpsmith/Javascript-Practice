9d0f73498abe050632dd15123c1cd10326e8afd5
Keaton Smith <keatonpsmith@gmail.com>
2019-01-13 16:43:52 -0500

    First four helper functions

--- Homework 1/src/reversi.js
+++ Homework 1/src/reversi.js
@@ -1,15 +1,38 @@
 function repeat(ele, n) {
-    // implementation
+    const array = [];
+    for (let i = 0; i < n; i++){
+        array.push(ele);
+    }
+    return array;
 } 
     
 function generateBoard(rows, cols, initialValue) {
-    // implementation
+    const sizeOfBoard = rows * cols
+    if(initialValue){
+        return repeat(initialValue, sizeOfBoard);
+    }
+    else{
+        return repeat(" ", sizeOfBoard);
+    }
 } 
 
-// ...
+function rowColToIndex(board, rowNumber, columnNumber) {
+    const sizeOfBoard = Math.sqrt(board.length);
+    return ((sizeOfBoard * rowNumber) + columnNumber);
+}
+
+function indexToRowCol(board, i) {
+    const sizeOfBoard = Math.sqrt(board.length);
+    const rowColumn = {};
+    rowCol.row = Math.floor(i / sizeOfBoard);
+    rowCol.col = i % sizeOfBoard;
+    return rowCol;
+}
 
 module.exports = {
     repeat: repeat,
     generateBoard: generateBoard,
+    rowColToIndex: rowColToIndex,
+    indexToRowCol: indexToRowCol,
     // ...
 }
